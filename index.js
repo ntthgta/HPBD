5dcd33ec8f485e0806095b20cdf41e9366875363
ntthgta <Nguyenthithuhoai1908@gmail.com>
2024-06-12 16:29:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const contentLetterSrart_actived = "Tadaaa món quà của ntth dành tặng cho anhh nè^^" //Lời mở đầu cho bức thư
-const mainContentLetter = "E hèm thay mặt Đảng,Chính phủ,các vị lãnh đạo và toàn thể nhân dân Việt Nam cùng hơn 7 tỷ người trên thế giới, em xin gửi lời chúc này đến anhh.
-    Chúc cho những ngày tháng tiếp theo của anh sẽ tràn đầy nắng ấm, những cơn mưa đến sẽ làm dịu tâm hồn sau 1 ngày làm việc vất vả. Tuổi mới mong anh sẽ luôn rõ ràng với những gì anh đã chọn.
-    Ntth chúc ATus có 1 ngày sinh vuive!!!!" //Nội dung của bức thư
+const mainContentLetter = "E hèm thay mặt Đảng,Chính phủ,các vị lãnh đạo và toàn thể nhân dân Việt Nam cùng hơn 7 tỷ người trên thế giới, em xin gửi lời chúc này đến anh.Chúc anh tuổi mới đong đầy,bao nhiêu điều tốt đều là của anh. Chúc cho những tháng ngày sắp tới,mỗi ngày của anh sẽ tràn ngập nắng ấm,những cơn mưa có đến thì nó cũng sẽ làm dịu đi những mệt mỏi mà anh phải chịu sau một ngày làm việc vất vả. Tuổi mới mong anh sẽ luôn rõ ràng với những gì mà anh đã chọn và sớm kiếm được mối tình đầu cũng như cuối nhé :> .Cuối cùng Ntth chúc ATus có 1 ngày sinh vuive!!!!" //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
